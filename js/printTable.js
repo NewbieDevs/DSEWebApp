f19d6ef17edc6cd429042690cc0bb68cf97d7313
mayurkhandate <mayurkhandate789@gmail.com>
2020-09-21 23:02:12 +0530

    new cutoff section updated

--- js/printTable.js
+++ js/printTable.js
@@ -3,8 +3,8 @@ function loadTable(fname) {
     $.getJSON("../js/" + fname, function(json) {
             console.log("Working");
             var table1obj = json;
-            var tbl = $("<table class='table table-hover table-dark table-bordered'/>").attr("id", "mytable");
-            var tbHead = "<tr class='thead-dark'>" + "<th>Sr No</th>" + "<th>Institute Code</th>" + "<th>Institute Name</th>" + "<th>CAP I</th>" + "<th>CAP II</th>" + "<th>CAP III</th>" + "</tr>";
+            var tbl = $("<table class='table table-hover table-bordered'/>").attr("id", "mytable");
+            var tbHead = "<tr class='thead-cutoff'>" + "<th>Sr No</th>" + "<th>Institute Code</th>" + "<th>Institute Name</th>" + "<th>CAP I</th>" + "<th>CAP II</th>" + "<th>CAP III</th>" + "</tr>";
 
             //alert(JSON.stringify(table1obj))
             //$("#mytable").setAttribute("class", "table-hover");
@@ -16,12 +16,12 @@ function loadTable(fname) {
                 var td1 = "<td>" + cnt + "</td>";
                 var td2 = "<td>" + table1obj[i]["InstiCode"] + "</td>";
                 var td3 = "<td>" + table1obj[i]["InstiName"] + "</td>";
-                var td4 = "<td>" + "<a target='_blank' href='../assets/DiplomaCollegesCutoff/cap1/" + table1obj[i]["InstiCode"] +
-                    ".pdf'>VIEW</a>" + "</td>";
-                var td5 = "<td>" + "<a target='_blank' href='../assets/DiplomaCollegesCutoff/cap2/" + table1obj[i]["InstiCode"] +
-                    ".pdf'>VIEW</a>" + "</td>";
-                var td6 = "<td>" + "<a target='_blank' href='../assets/DiplomaCollegesCutoff/cap3/" + table1obj[i]["InstiCode"] +
-                    ".pdf'>VIEW</a>" + "</td></tr>";
+                var td4 = "<td>" + "<button class='btn btn-cutoff'><a target='_blank' href='../assets/DiplomaCollegesCutoff/cap1/" + table1obj[i]["InstiCode"] +
+                    ".pdf'>VIEW</a></button>" + "</td>";
+                var td5 = "<td>" + "<button class='btn btn-cutoff'><a target='_blank' href='../assets/DiplomaCollegesCutoff/cap2/" + table1obj[i]["InstiCode"] +
+                    ".pdf'>VIEW</a></button>" + "</td>";
+                var td6 = "<td>" + "<button class='btn btn-cutoff'><a target='_blank' href='../assets/DiplomaCollegesCutoff/cap3/" + table1obj[i]["InstiCode"] +
+                    ".pdf'>VIEW</a></button>" + "</td></tr>";
 
 
                 $("#mytable").append(tr + td1 + td2 + td3 + td4 + td5 + td6);
