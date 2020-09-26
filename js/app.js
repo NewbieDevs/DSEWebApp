c79b9eb778b8a0dc010c03575a1417e7bf90597d
coderjojo <mail2vedj@gmail.com>
2020-09-27 00:48:11 +0530

    Feat: Added favicon and fix scripts

---
+++ js/app.js
@@ -0,0 +1,13 @@
+       var firebaseConfig = {
+           apiKey: "AIzaSyA2ApQ3TISnBCnMU8cZo2LO70Uw4_fjvmg",
+           authDomain: "dsyhelper.firebaseapp.com",
+           databaseURL: "https://dsyhelper.firebaseio.com",
+           projectId: "dsyhelper",
+           storageBucket: "dsyhelper.appspot.com",
+           messagingSenderId: "974966995405",
+           appId: "1:974966995405:web:f7978d81f7b338052cac84",
+           measurementId: "G-30WZET7Z6K"
+       };
+       // Initialize Firebase
+       firebase.initializeApp(firebaseConfig);
+       firebase.analytics();
